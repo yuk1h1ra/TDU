d70e059f750934b352bb11d91a56dbccf2689d51
yuk1h1ra <koheishoji913@gmail.com>
2017-12-13 11:31:47 +0900

    add roop about define function or calclate strings

--- compiler/MyCalcurator.java
+++ compiler/MyCalcurator.java
@@ -7,6 +7,7 @@ class Calcurator {
             System.out.println("何をしますか？(Ctrl-dで終了)");
             System.out.println("1: 関数を定義する");
             System.out.println("2: 式の値を求める");
+            System.out.println("0: 終了");
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String line = null;
@@ -21,14 +22,17 @@ class Calcurator {
                 break;
             }
 
-            if(line == "1") {
+            if(line.equals("1")) {
                 
             }
-            else if (line == "2") {
+            else if(line.equals("2")) {
                 
+            }
+            else if(line.equals("0")) {
+                System.exit(0);
             }
             else {
-                System.out.println("１または２を代入してください");
+                System.out.println("1または2を代入してください");
             }
         }
     }
