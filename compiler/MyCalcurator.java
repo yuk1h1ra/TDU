b371b9c081b65c54f4391d9d7d7ecdeeae7e641d
yuk1h1ra <koheishoji913@gmail.com>
2017-12-13 16:37:39 +0900

    create Token class

--- compiler/MyCalcurator.java
+++ compiler/MyCalcurator.java
@@ -1,6 +1,50 @@
 import java.io.*;
 import java.util.*;
 
+abstract class Token implements Comparable<Token> {
+    private String string;
+    protected int kind;
+
+    public static final int START = 0;
+    public static final int SUM = 1;
+    public static final int SUB = 2;
+    public static final int MUL = 3;
+    public static final int DIV = 4;
+    public static final int LPAREN = 5;
+    public static final int RPAREN = 6;
+    public static final int OPERAND = 7;
+    public static final int END = 8;
+
+    private static final int order[][] = {
+        // 始  +  -  *  /  (  ) 識別子 終 <-右  左
+        {   9, 1, 1, 1, 1, 1, 9, 1,     0 }, // 始
+        {   9,-1,-1, 1, 1, 1,-1, 1,    -1 }, // +
+        {   9,-1,-1, 1, 1, 1,-1, 1,    -1 }, // -
+        {   9,-1,-1,-1,-1, 1,-1, 1,    -1 }, // *
+        {   9,-1,-1,-1,-1, 1,-1, 1,    -1 }, // /
+        {   9, 1, 1, 1, 1, 1, 0, 1,     9 }, // (
+        {   9, 9, 9, 9, 9, 9, 9, 9,     9 }, // )
+        {   9,-1,-1,-1,-1, 9,-1, 9,    -1 }, // 識別子
+        {   9, 9, 9, 9, 9, 9, 9, 9,     9 }, // 終
+    };
+
+    public Token(String string) {
+        this.string = string;
+    }
+
+    public String toString() {
+        return string;
+    }
+
+    public int getKind() {
+        return kind;
+    }
+
+    public int compareTo(Token anotherToken) {
+        return order[this.kind][anotherToken.getKind()];
+    }
+}
+
 class Calcurator {
     public Calcurator() {
         while(true) {
